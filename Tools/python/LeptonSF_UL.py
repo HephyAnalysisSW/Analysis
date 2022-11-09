2c1fe0ad1522f1c8b33489e962bb40745f0ae330
Maryam Shooshtari <mary.shooshtari@gmail.com>
2022-11-09 17:33:26 +0100

    Lepton SF

--- Tools/python/LeptonSF_UL.py
+++ Tools/python/LeptonSF_UL.py
@@ -1,9 +1,9 @@
 import ROOT
 import os
-from math import sqrt
+
 
 from Analysis.Tools.helpers import getObjFromFile
-from Analysis.Tools.u_float import u_float
+
 
 
 maps_ele =           {"medium": "egammaEffi_EGM2D_Medium.root",
@@ -12,31 +12,31 @@ maps_ele =           {"medium": "egammaEffi_EGM2D_Medium.root",
                       "Vloose": "egammaEffi_EGM2D_VLoose.root",
                      }
 
-maps_mu =            {"tight":  "NUM_LeptonMvaTight_DEN_TrackerMuons_abseta_pt.root",
-                      "medium": "NUM_LeptonMvaMedium_DEN_TrackerMuons_abseta_pt.root",
-                      "loose":  "NUM_LeptonMvaLoose_DEN_TrackerMuons_abseta_pt.root",
-                      "Vloose": "NUM_LeptonMvaVLoose_DEN_TrackerMuons_abseta_pt.root",
+maps_mu =            {"tight":  [("NUM_LeptonMvaTight_DEN_TrackerMuons_abseta_pt.root","NUM_LeptonMvaTight_DEN_TrackerMuons_abseta_pt")],
+                      "medium": [("NUM_LeptonMvaMedium_DEN_TrackerMuons_abseta_pt.root","NUM_LeptonMvaMedium_DEN_TrackerMuons_abseta_pt")],
+                      "loose":  [("NUM_LeptonMvaLoose_DEN_TrackerMuons_abseta_pt.root","NUM_LeptonMvaLoose_DEN_TrackerMuons_abseta_pt")],
+                      "Vloose": [("NUM_LeptonMvaVLoose_DEN_TrackerMuons_abseta_pt.root","NUM_LeptonMvaVLoose_DEN_TrackerMuons_abseta_pt")],
                      }
 
-class leptonSF:
-    def __init__(self, year, ID = None):
+class LeptonSF:
+    def __init__(self, era, ID = None):
 
-        if year not in ["2016_HIPM", "2016", "2017", "2018" ]:
-            raise Exception("Lepton SF for year %i not known"%year)
+        if era not in ["UL2016_preVFP", "UL2016", "UL2017", "UL2018" ]:
+            raise Exception("Lepton SF for era %i not known"%era)
 
         self.dataDir = "$CMSSW_BASE/src/Analysis/Tools/data/leptonSFData/LeptonMva_v1"
-        self.year = year
+        self.era = era
 
         self.SFmaps = {
             "elec" : {
-                "SF" :  getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_el[ID],"EGamma_SF2D"),
-                "syst": getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_el[ID],"sys"),
-                "stat": getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_el[ID],"stat"),
+                "SF" :  getObjFromFile(self.dataDir+"/"+self.era+"/"+maps_el[ID],"EGamma_SF2D"),
+                "syst": getObjFromFile(self.dataDir+"/"+self.era+"/"+maps_el[ID],"sys"),
+                "stat": getObjFromFile(self.dataDir+"/"+self.era+"/"+maps_el[ID],"stat"),
             },
             "muon" : {
-                "SF" :  getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_mu[ID],"NUM_LeptonMvaTight_DEN_TrackerMuons"),
-                "syst": getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_mu[ID],"syst"),
-                "stat": getObjFromFile(self.dataDir+"/"+self.year+"/"+maps_mu[ID],"stat"),
+                "SF" :  [getObjFromFile(os.path.expandvars(os.path.join(self.dataDir+"/"+self.era, file)), key) for (file, key) in maps_mu[ID]],
+                "syst": [getObjFromFile(os.path.expandvars(os.path.join(self.dataDir+"/"+self.era, file)), key+"_syst") for (file, key) in maps_mu[ID]],
+                "stat": [getObjFromFile(os.path.expandvars(os.path.join(self.dataDir+"/"+self.era, file)), key+"_stat") for (file, key) in maps_mu[ID]],
             }
         }
 
@@ -55,6 +55,12 @@ class leptonSF:
             if pt > 200:
                 pt = 199
 
+            etabin = self.SFmaps[lepton]["SF"].GetXaxis().FindBin(eta)
+            ptbin  = self.SFmaps[lepton]["SF"].GetYaxis().FindBin(pt)
+            SF = self.SFmaps[lepton]["SF"].GetBinContent(etabin, ptbin)
+            err = self.SFmaps[lepton][uncert].GetBinContent(etabin, ptbin)
+
+
         elif abs(pdgId)==13:
             lepton = "muon"
             eta = abs(eta)
@@ -62,11 +68,12 @@ class leptonSF:
                 eta = 2.39
             if pt > 120:
                 pt = 119
+            etabin = self.SFmaps[lepton]["SF"][0].GetXaxis().FindBin(eta)
+            ptbin  = self.SFmaps[lepton]["SF"][0].GetYaxis().FindBin(pt)
+            SF = self.SFmaps[lepton]["SF"][0].GetBinContent(etabin, ptbin)
+            err = self.SFmaps[lepton][uncert][0].GetBinContent(etabin, ptbin)
+
         else:
           raise Exception("Lepton SF for PdgId %i not known"%pdgId)
 
-        etabin = self.SFmaps[lepton]["SF"].GetXaxis().FindBin(eta)
-        ptbin  = self.SFmaps[lepton]["SF"].GetYaxis().FindBin(pt)
-        SF = self.SFmaps[lepton]["SF"].GetBinContent(etabin, ptbin)
-        err = self.SFmaps[lepton][uncert].GetBinContent(etabin, ptbin)
         return SF+sigma*err
